05d201d9bf1342010a0ba558939ae153b9531da0
Arnold <arnoldtee01@gmail.com>
2025-06-14 15:09:58 +0200

    added hero section paragraph text

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ function App() {
       <main>
         <section className='hero'>
           <h1 className='hero-text'>ESSENCE OF CHAMPAGNE</h1>
+          <p className='hero-p'>Lorem Ipsum epitomizes champagne craftsmanship with excellence in vineyard practices, blending the finest grapes for complexity and balance. It's sensory journey features ripe fruits, toasted nuts, and a lingering finish, perfect for special occasions.</p>
         </section>
       </main>
     </>
