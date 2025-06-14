672a6c6ef1f9d34a7d22d182f02dabfb995fc62f
Arnold <arnoldtee01@gmail.com>
2025-06-14 16:02:21 +0200

    added first scrolling best

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,16 @@ function App() {
       <main>
         <section className='hero'>
           <h1 className='hero-text'>ESSENCE OF CHAMPAGNE</h1>
+
           <p className='hero-p'>Lorem Ipsum epitomizes champagne craftsmanship with excellence in vineyard practices, blending the finest grapes for complexity and balance. It's sensory journey features ripe fruits, toasted nuts, and a lingering finish, perfect for special occasions.</p>
+
+          <div className="scrolling-belt-container">
+            <div className="scrolling-belt">
+              <span>Lorem Ipsum's · Lorem Ipsum's · Lorem Ipsum's · Lorem Ipsum's · </span>
+              <span>Lorem Ipsum's · Lorem Ipsum's · Lorem Ipsum's · Lorem Ipsum's · </span>
+            </div>
+          </div>
+
         </section>
       </main>
     </>
